3e90c63fc90ba1f695f22e02beb7893c35896f18
Shahid Akhtar <shahidakhtar016@gmail.com>
2021-06-08 17:12:39 +0530

    Layout improved and form added in main component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import Main from './components/Main/Main';
 import Details from './components/Details/Details';
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+
     return ( 
     <div>
-        <Grid container spacing={0} alignItems="center" justify="center" style={{ height: `100vh` }}>
+        <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: `100vh` }}>
             <Grid item xs={12} sm={4}>
-                <Details />
+                <Details title="Income" />
+            </Grid>
+            <Grid item xs={12} sm={3}>
+                <Main/>
             </Grid>
             <Grid item xs={12} sm={4}>
-                Main Component
-            </Grid>
-            <Grid item xs={12} sm={4}>
-                <Details />
+                <Details title="Expense" />
             </Grid>
         </Grid>
     </div>
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+} from "@material-ui/core";
+import Form from "./Form/Form";
+import useStyles from "./styles";
+
+const Main = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Card className={classes.root}>
+        <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+        <CardContent>
+          <Typography align="center" variant="h5">
+            Total Balance $100
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            styles={{ lineHeight: "1.5em", marginTop: "20px" }}
+          >
+            {/* Info component will get added here */}
+            Try saying: Add income for $100 in Category Slary for Monday...
+          </Typography>
+          <Divider />
+          <Form />
+        </CardContent>
+        <CardContent className={classes.cardContent}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}></Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Main;
