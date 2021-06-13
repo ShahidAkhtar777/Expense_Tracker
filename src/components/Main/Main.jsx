6ddbf52321f4983177ecf77a58632c7c1344012d
Shahid Akhtar <shahidakhtar016@gmail.com>
2021-06-13 11:38:37 +0530

    Rendered data in chart

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -16,32 +16,30 @@ const Main = () => {
   const classes = useStyles();
 
   return (
-    <div>
-      <Card className={classes.root}>
-        <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
-        <CardContent>
-          <Typography align="center" variant="h5">
-            Total Balance $100
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            styles={{ lineHeight: "1.5em", marginTop: "20px" }}
-          >
-            {/* Info component will get added here */}
-            Try saying: Add income for $100 in Category Slary for Monday...
-          </Typography>
-          <Divider />
-          <Form />
-        </CardContent>
-        <CardContent className={classes.cardContent}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <List />
-            </Grid>
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+      <CardContent>
+        <Typography align="center" variant="h5">
+          Total Balance $100
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          styles={{ lineHeight: "1.5em", marginTop: "20px" }}
+        >
+          {/* Info component will get added here */}
+          Try saying: Add income for $100 in Category Salary for Monday...
+        </Typography>
+        <Divider />
+        <Form />
+      </CardContent>
+      <CardContent className={classes.cardContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <List />
           </Grid>
-        </CardContent>
-      </Card>
-    </div>
+        </Grid>
+      </CardContent>
+    </Card>
   );
 };
 
