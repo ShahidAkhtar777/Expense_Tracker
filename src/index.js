72ea8f2722ced78ca2f98a90c35a5671fb3e9bc5
Shahid Akhtar <shahidakhtar016@gmail.com>
2021-06-14 22:47:09 +0530

    Added Speechly to fill form data using speech

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SpeechProvider } from '@speechly/react-client';
 
 import { Provider } from './context/context';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
+    <SpeechProvider appId="85cfbc38-ce7e-4a08-8676-29273d21c34e" language="en-US">
     <Provider>
         <App />
-    </Provider>,
+    </Provider>
+    </SpeechProvider>,
 document.getElementById('root'));
